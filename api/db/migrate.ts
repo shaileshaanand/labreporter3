61dd65411ecf21c9bcfd8ccad72276834753eb0c
Shailesh Aanand <anaandshailu@gmail.com>
2024-10-11 08:39:06 +0530

    fix migrator for pglite

--- api/db/migrate.ts
+++ api/db/migrate.ts
@@ -1,8 +1,19 @@
-import { migrate } from "drizzle-orm/node-postgres/migrator";
-import type { NodePgDatabase } from "../utils/db";
+import { migrate as nodePgMigrate } from "drizzle-orm/node-postgres/migrator";
+import { migrate as pgLiteMigrate } from "drizzle-orm/pglite/migrator";
+// import type { NodePgDatabase } from "../utils/db";
+import type * as schema from "../db/schema";
+import type { PgliteDatabase } from "drizzle-orm/pglite";
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
 
-const migrateDb = (db: NodePgDatabase) => {
-  return migrate(db, {
+const migrateDb = (
+  db: NodePgDatabase<typeof schema> | PgliteDatabase<typeof schema>,
+) => {
+  if (db instanceof NodePgDatabase) {
+    return nodePgMigrate(db, {
+      migrationsFolder: "api/db/drizzle",
+    });
+  }
+  return pgLiteMigrate(db, {
     migrationsFolder: "api/db/drizzle",
   });
 };
