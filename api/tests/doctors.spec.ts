16e95164ffd27dd15a81560919284c4cbe657a0b
Shailesh Aanand <anaandshailu@gmail.com>
2024-10-11 16:49:03 +0530

    add doctor tests

--- api/tests/doctors.spec.ts
+++ api/tests/doctors.spec.ts
@@ -4,9 +4,12 @@ import { treaty } from "@elysiajs/eden";
 import { faker } from "@faker-js/faker";
 import { eq } from "drizzle-orm";
 import { type PgliteDatabase, drizzle } from "drizzle-orm/pglite";
+import { SignJWT } from "jose";
 import app from "../app";
 import migrateDb from "../db/migrate";
 import * as schema from "../db/schema";
+import env from "../env";
+import { doctorFactory } from "./factories";
 import { generatePhoneNumber } from "./utils";
 
 let db: PgliteDatabase<typeof schema>;
@@ -14,10 +17,14 @@ let dbClient: PGlite;
 
 const api = treaty(app).api;
 
+const authHeader = `Bearer ${await new SignJWT({ id: 1 })
+  .setProtectedHeader({ alg: "HS256" })
+  .sign(new TextEncoder().encode(env.JWT_SECRET))}`;
+
 describe("Doctor Tests", () => {
   beforeEach(async () => {
     dbClient = new PGlite();
-    db = drizzle(dbClient);
+    db = drizzle(dbClient, { schema });
     await migrateDb(db);
     mock.module("../utils/db.ts", () => {
       return { default: db };
@@ -34,7 +41,11 @@ describe("Doctor Tests", () => {
       phone: generatePhoneNumber(),
       email: faker.internet.email(),
     };
-    const response = await api.doctors.index.post(payload);
+    const response = await api.doctors.index.post(payload, {
+      headers: {
+        Authorization: authHeader,
+      },
+    });
 
     expect(response.status).toBe(201);
     if (response.error) {
@@ -52,15 +63,67 @@ describe("Doctor Tests", () => {
     expect(createdDoctor.name).toBe(payload.name);
     expect(createdDoctor.deleted).toBe(false);
   });
+
   it("Should get all doctors", async () => {
-    const response = await api.doctors.index.post({
-      name: faker.person.fullName(),
-      phone: generatePhoneNumber(),
-      email: faker.internet.email(),
+    const doctorResults = await Promise.all([
+      doctorFactory(db),
+      doctorFactory(db),
+      doctorFactory(db),
+    ]);
+
+    const response = await api.doctors.index.get({
+      headers: { Authorization: authHeader },
     });
+
+    expect(response.status).toBe(200);
     if (response.error) {
       throw response.error;
     }
-    response.data;
+    const { data } = response;
+
+    for (const doctor of doctorResults) {
+      const foundDoctor = data.find((d) => {
+        return d.id === doctor.id;
+      });
+      if (!foundDoctor) {
+        throw new Error("Doctor not found");
+      }
+      expect(foundDoctor.name).toBe(doctor.name);
+      expect(foundDoctor.email).toBe(doctor.email);
+      expect(foundDoctor.phone).toBe(doctor.phone);
+    }
+  });
+
+  it("Should not get deleted doctors", async () => {
+    //create 3 doctors, 1 of them deleted
+    const doctors = await Promise.all([doctorFactory(db), doctorFactory(db)]);
+    const deletedDoctor = await doctorFactory(db, { deleted: true });
+    const response = await api.doctors.index.get({
+      headers: { Authorization: authHeader },
+    });
+    expect(response.status).toBe(200);
+    if (response.error) {
+      throw response.error;
+    }
+    const { data } = response;
+    expect(data.length).toBe(doctors.length);
+    for (const doctor of doctors) {
+      const foundDoctor = data.find((d) => {
+        return d.id === doctor.id;
+      });
+      if (!foundDoctor) {
+        throw new Error("doctor not found");
+      }
+      expect(foundDoctor.name).toBe(doctor.name);
+      expect(foundDoctor.email).toBe(doctor.email);
+      expect(foundDoctor.phone).toBe(doctor.phone);
+    }
+  });
+
+  it("Should not get doctors if unauthorized", async () => {
+    const doctor = await doctorFactory(db, { deleted: true });
+    const response = await api.doctors.index.get();
+    expect(response.status).toBe(401);
+    expect(response.error).toBeDefined();
   });
 });
---
+++ api/tests/factories.ts
@@ -0,0 +1,175 @@
+import { faker } from "@faker-js/faker";
+import type { BunSQLiteDatabase } from "drizzle-orm/bun-sqlite";
+import type { NodePgDatabase } from "drizzle-orm/node-postgres";
+import type { PgliteDatabase } from "drizzle-orm/pglite";
+import * as schema from "../db/schema";
+import { hashPassword } from "../utils/passwords";
+import { generatePhoneNumber } from "./utils";
+
+export const doctorFactory = async (
+  db: NodePgDatabase<typeof schema> | PgliteDatabase<typeof schema>,
+  {
+    name = null,
+    email = null,
+    phone = null,
+    deleted = false,
+  }: {
+    name?: string | null;
+    email?: string | null;
+    phone?: string | null;
+    deleted?: boolean;
+  } = {
+    name: null,
+    email: null,
+    phone: null,
+    deleted: false,
+  },
+) => {
+  const [doctor] = await db
+    .insert(schema.doctors)
+    .values({
+      name: name ?? faker.person.fullName(),
+      phone: phone ?? generatePhoneNumber(),
+      email: email ?? faker.internet.email(),
+      deleted,
+    })
+    .returning();
+
+  return doctor as Exclude<typeof doctor, undefined>;
+};
+
+export const userFactory = async (
+  db: NodePgDatabase<typeof schema> | PgliteDatabase<typeof schema>,
+  {
+    firstName = null,
+    lastName = null,
+    username = null,
+    password,
+    deleted = false,
+  }: {
+    firstName?: string | null;
+    lastName?: string | null;
+    username?: string | null;
+    password: string;
+    deleted?: boolean;
+  },
+) => {
+  const [user] = await db
+    .insert(schema.users)
+    .values({
+      firstName: firstName ?? faker.person.firstName(),
+      lastName: lastName ?? faker.person.lastName(),
+      username: username ?? faker.internet.userName(),
+      passwordHash: await hashPassword(password),
+      deleted,
+    })
+    .returning();
+
+  return user as Exclude<typeof user, undefined>;
+};
+
+export const patientFactory = async (
+  db: NodePgDatabase<typeof schema> | PgliteDatabase<typeof schema>,
+  {
+    name = null,
+    email = null,
+    phone = null,
+    age = null,
+    gender = null,
+    deleted = false,
+  }: {
+    name?: string | null;
+    email?: string | null;
+    phone?: string | null;
+    age?: number | null;
+    gender?: "male" | "female" | null;
+    deleted?: boolean;
+  } = {
+    name: null,
+    email: null,
+    phone: null,
+    age: null,
+    gender: null,
+    deleted: false,
+  },
+) => {
+  const [patient] = await db
+    .insert(schema.patients)
+    .values({
+      name: name ?? faker.person.fullName(),
+      phone: phone ?? generatePhoneNumber(),
+      email: email ?? faker.internet.email(),
+      age: age ?? faker.number.int({ min: 0, max: 120 }),
+      gender: gender ?? faker.helpers.arrayElement(["male", "female"]),
+      deleted,
+    })
+    .returning();
+  return patient as Exclude<typeof patient, undefined>;
+};
+
+export const templateFactory = async (
+  db: NodePgDatabase<typeof schema> | PgliteDatabase<typeof schema>,
+  {
+    name = null,
+    content = null,
+    deleted = false,
+  }: {
+    name?: string | null;
+    content?: string | null;
+    deleted?: boolean;
+  } = {
+    name: null,
+    deleted: false,
+  },
+) => {
+  const [template] = await db
+    .insert(schema.templates)
+    .values({
+      name: name ?? faker.lorem.sentence(),
+      content: content ?? faker.lorem.paragraph(),
+      deleted,
+    })
+    .returning();
+
+  return template as Exclude<typeof template, undefined>;
+};
+
+// export const USGReportFactory = async (
+//   db: NodePgDatabase<typeof schema>|PgliteDatabase<typeof schema>,
+//   {
+//     patientId = null,
+//     referrerId = null,
+//     partOfScan = null,
+//     findings = null,
+//     date = null,
+//     deleted = false,
+//   }: {
+//     patientId?: number | null;
+//     referrerId?: number | null;
+//     partOfScan?: string | null;
+//     findings?: string | null;
+//     date?: Date | null;
+//     deleted?: boolean;
+//   } = {
+//     patientId: null,
+//     referrerId: null,
+//     partOfScan: null,
+//     findings: null,
+//     date: null,
+//     deleted: false,
+//   },
+// ) => {
+//   const [USGReport] = await db
+//     .insert(schema.USGReports)
+//     .values({
+//       patientId: patientId ?? (await patientFactory(db)).id,
+//       referrerId: referrerId ?? (await doctorFactory(db)).id,
+//       partOfScan: partOfScan ?? faker.lorem.sentence(),
+//       findings: findings ?? faker.lorem.paragraph(),
+//       date: date ?? faker.date.recent({ days: 30 }),
+//       deleted,
+//     })
+//     .returning();
+
+//   return USGReport;
+// };
