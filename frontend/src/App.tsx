5d603be1377c6476829f5a1293e8fbc791b319d4
Shailesh Aanand <anaandshailu@gmail.com>
2024-10-13 17:38:30 +0530

    Homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,41 @@
-import { useEffect, useState } from "react";
-
-import type { Api } from "../../api/app";
-
-import { treaty } from "@elysiajs/eden";
-
+import { ClipboardPlus, Home, Users } from "lucide-react";
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const api = treaty<Api>(window.location.host).api;
-    api.doctors.index.get().then((response) => {
-      if (response.error) {
-        throw response.error;
-      }
-      console.log(response.data);
-    });
-  }, []);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="grid min-h-screen w-full bg-background md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden border-r bg-muted/40 md:block">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <div className="flex items-center gap-2 font-semibold w-full">
+              <span className="text-2xl w-full text-center">Lab Reporter</span>
+            </div>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <div className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                <Home className="h-4 w-4" />
+                Home
+              </div>
+              <div className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary">
+                <ClipboardPlus className="h-4 w-4" />
+                Reports
+              </div>
+              <div className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                <Users className="h-4 w-4" />
+                Patients
+              </div>
+              <div className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                Doctors
+              </div>
+              <div className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                Templates
+              </div>
+              <div className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                Analytics
+              </div>
+            </nav>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
